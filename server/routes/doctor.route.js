74abf948a72022b193c4affe716945d26fe4d4d0
NoobProgrammer <nguyenhotrunghieu0106@gmail.com>
2022-07-16 22:29:09 +0700

    Adjust models, CRUD patients in doctor route, write API doc

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -3,6 +3,7 @@ const controller = require("../controllers/doctor.controller");
 
 // Patients related
 router.put("/patients/update/id=:id", controller.updatePatient);
+router.delete("/patients/delete/id=:id", controller.deletePatient);
 // router.put("/patients/account/id=:id", controller.editAccount);
 router.post("/patients", controller.registerAccount);
 router.get("/patients", controller.getAllPatients);
