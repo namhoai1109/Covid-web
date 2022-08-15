9902e4ba6f1680ef41da642efda11a5ec99e1bf4
Hiếu Nguyễn <nguyenhotrunghieu0106@gmail.com>
2022-08-15 15:49:10 +0700

    Add get doctor logs for admin + update api

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,5 +1,6 @@
 const Account = require("../models/Account");
 const Doctor = require("../models/Doctor");
+const Log = require("../models/Log");
 const bcrypt = require("bcryptjs");
 
 // Register doctors
@@ -71,3 +72,15 @@ exports.deleteAccount = async (req, res) => {
     res.status(400).send({ message: err.message });
   }
 };
+
+// Get doctor logs
+exports.getLogs = async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+    const logs = await Log.find({ account: doctor.account }).sort({ time: -1 });
+
+    res.send(logs);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -6,6 +6,7 @@ router.post("/register", controller.registerAccount);
 router.get("/doctors", controller.getAll);
 router.put("/doctors/id=:id/changestatus", controller.changeStatus);
 router.delete("/doctors/id=:id/delete", controller.deleteAccount);
+router.get("/doctors/id=:id/logs", controller.getLogs);
 // router.get("/admin", controller.getAdminInfo);
 
 
